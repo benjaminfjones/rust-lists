3bc130f6eaa8afe31ebd61054081e2d5ac2c4a14
Benjamin Jones <benjaminfjones@gmail.com>
2020-04-29 14:35:12 -0700

    Add more comments to first.rs and add a Drop impl test

--- src/first.rs
+++ src/first.rs
@@ -3,20 +3,29 @@
 
 use std::mem;
 
+//////////////////////////////////////////////////////////////////////////////
+// Types
+
+// A newtype around Link
 pub struct List {
     head: Link,
 }
 
+// An ADT the represents a link list Link, either the link is empty (end of the list) or it is a
+// pointer to a Node.
 enum Link {
     Empty,
     More(Box<Node>),
 }
 
+// A list node is an integer and a link.
+// TODO: parametrize type of 'elem'.
 struct Node {
     elem: i32,
     next: Link,
 }
 
+//////////////////////////////////////////////////////////////////////////////
 // Implementation
 
 impl List {
@@ -25,10 +34,12 @@ impl List {
         List { head: Link::Empty }
     }
 
-    // push an integer onto the given stack
+    // push an integer onto the stack
     pub fn push(&mut self, x: i32) {
         let new_box_node = Box::new(Node {
             elem: x,
+            // disassociate the old `self.head` from `self`, returning it's value and replace it
+            // with a new Link::empty indicating end of list.
             next: mem::replace(&mut self.head, Link::Empty),
         });
         self.head = Link::More(new_box_node);
@@ -36,6 +47,9 @@ impl List {
 
     // pop an integer from the stack, returning either Some(value) or None if
     // the stack is empty.
+    //
+    // Here the head of the list is matches on and replaced with Link::Empty. In case there is an
+    // element to pop, the head is reassigned to point to the next element.
     pub fn pop(&mut self) -> Option<i32> {
         match mem::replace(&mut self.head, Link::Empty) {
             // replaced value is same as original
@@ -56,6 +70,7 @@ impl Drop for List {
     fn drop(&mut self) {
         let mut cur_link = mem::replace(&mut self.head, Link::Empty);
         // `while let` == "do this thing until this pattern doesn't match"
+        // `boxed_node` is matches as `mut` so that we can extract and replace its `next` field.
         while let Link::More(mut boxed_node) = cur_link {
             cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
             // boxed_node goes out of scope and gets dropped here;
@@ -97,5 +112,16 @@ mod test {
 
         // test for exhaustion
         assert_eq!(list.pop(), None);
+    }
+
+    // If the Drop impl for List is commented out above, this test will cause the stack to
+    // overflow.
+    #[test]
+    fn test_drop() {
+        let mut list = List::new();
+        for i in 0..1000000 {
+            list.push(i);
+        }
+        // list is dropped
     }
 }
